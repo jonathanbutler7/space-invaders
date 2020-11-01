199bdcaf5ccce8a7f42732ed4fd80038a1809da5
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-10-31 22:39:24 -0700

    added eenmy class

--- main.py
+++ main.py
@@ -52,6 +52,24 @@ class Player(Ship):
         self.ship_img = YELLOW_SPACE_SHIP
         self.laser_img = YELLOW_LASER
         self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
+    
+
+class Enemy(Ship):
+    COLOR_MAP = {
+                "red": (RED_SPACE_SHIP, RED_LASER),
+                "green": (GREEN_SPACE_SHIP, GREEN_LASER),
+                "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
+                }
+
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
+
     
 def main():
     run = True
