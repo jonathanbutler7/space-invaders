2ff8cfbc6c4d0cbad627246ff81bbe4908c7ed47
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-10-31 21:09:15 -0700

    added width, height

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 import time
 import random
 
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter")
+
 # Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
@@ -19,3 +23,4 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 # Background
 BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+
